ab3500c1cbef8bd051284744862cc8b763525720
John Paxton <johnpaxton@gmail.com>
2022-10-27 10:12:25 -0400

    Add insert and delete to example

--- db/demos/authors-demo.js
+++ db/demos/authors-demo.js
@@ -7,6 +7,19 @@ async function main() {
     await connection.sync();
     let robertParker = await Author.findByPk(5);
     console.log('Found the author:', robertParker.commonName);
+
+    let douglasAdams = Author.build({
+      firstName: 'Douglas',
+      lastName: 'Adams',
+      commonName: 'Douglas Adams',
+    });
+
+    await douglasAdams.save();
+    console.log('Douglas Adams added to the database with id', douglasAdams.authorId);
+
+    await douglasAdams.destroy();
+    console.log('Douglas Adams removed from the database');
+
     await connection.close();
   } catch (error) {
     console.error('Database error:', error);
