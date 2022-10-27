a9b7ceaa7468be866503c0924a6a0188013ed153
John Paxton <johnpaxton@gmail.com>
2022-10-27 10:01:19 -0400

    More comments

--- server/orm/models/Book.js
+++ server/orm/models/Book.js
@@ -1,7 +1,9 @@
 import { DataTypes } from 'sequelize';
 import { connection } from '../lcs-connection.js';
 
-const Book = connection.define('book', {
+// The name of the model should be a singluar version of the name of the table
+// e.g. The model is Book which connects to the table "books"
+const Book = connection.define('Book', {
   bookId: {
     type: DataTypes.INTEGER,
     primaryKey: true,
